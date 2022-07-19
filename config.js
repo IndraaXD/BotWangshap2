433f33ef7e14515d4cf63e8d7637fe78ad0ffda5
IndraXD <81459543+IndraaXD@users.noreply.github.com>
2022-07-19 16:31:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to o
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
 global.autoRecord = true //auto recording (true to on, false to off)
-global.available = true false//auto available (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
